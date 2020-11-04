deab721f78b8a92e5e2ffa101048d2ae16affac6
uty <whensungoes@gmail.com>
2020-11-04 20:38:43 +0000

    print input output in each layer

--- testmnist.py
+++ testmnist.py
@@ -16,21 +16,6 @@ test_loader = torch.utils.data.DataLoader(
   batch_size=batch_size_test, shuffle=True)
 
 
-examples = enumerate(test_loader)
-batch_idx, (example_data, example_targets) = next(examples)
-
-
-fig = plt.figure()
-for i in range(6):
-  plt.subplot(2,3,i+1)
-  plt.tight_layout()
-  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
-  plt.title("Ground Truth: {}".format(example_targets[i]))
-  plt.xticks([])
-  plt.yticks([])
-fig
-
-plt.show()
 
 
 # Specify a path
@@ -42,6 +27,84 @@ model.eval()
 
 print(model)
 util.print_model_parameters(model)
+
+
+
+examples = enumerate(test_loader)
+batch_idx, (example_data, example_targets) = next(examples)
+
+
+fig = plt.figure()
+#for i in range(6):
+#  plt.subplot(2,3,i+1)
+for i in range(1):
+  plt.tight_layout()
+  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+  plt.title("Ground Truth: {}".format(example_targets[i]))
+  plt.xticks([])
+  plt.yticks([])
+fig
+
+plt.show()
+
+
+#test
+def printnorm(self, input, output):
+    # input is a tuple of packed inputs
+    # output is a Tensor. output.data is the Tensor we are interested
+    print('Inside ' + self.__class__.__name__ + ' forward')
+    print('')
+    print('input: ', type(input))
+    print(input)
+    print('input[0]: ', type(input[0]))
+    print('output: ', type(output))
+    print(output)
+    print('')
+    print('input size:', input[0].size())
+    print('output size:', output.data.size())
+    print('output norm:', output.data.norm())
+
+
+model.fc1.register_forward_hook(printnorm)
+model.fc2.register_forward_hook(printnorm)
+model.fc3.register_forward_hook(printnorm)
+#
+
+
+result = model(example_data[0])
+pred = result.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+print("\npruned model predict:")
+print(pred)
+print("\n")
+
+
+
+
+
+
+
+
+# Specify a path
+PATH_ORIG = "saves/initial_model.ptmodel"
+
+# Load
+model_orig = torch.load(PATH_ORIG)
+model_orig.eval()
+
+print(model_orig)
+util.print_model_parameters(model_orig)
+
+model_orig.fc1.register_forward_hook(printnorm)
+model_orig.fc2.register_forward_hook(printnorm)
+model_orig.fc3.register_forward_hook(printnorm)
+
+result_orig = model_orig(example_data[0])
+pred_orig = result_orig.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+print("\noriginal model predict:")
+print(pred_orig)
+print("\n")
+
+
 
 
 
@@ -66,5 +129,6 @@ def test():
     return accuracy
 
 
-accuracy = test()
-print(accuracy)
+#accuracy = test()
+#print(accuracy)
+
