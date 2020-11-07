6cba52126d6c177b036372c40b5c5a81b6c50733
uty <whensungoes@gmail.com>
2020-11-07 01:58:06 +0000

    show histogram on tensorboard

--- testmnist.py
+++ testmnist.py
@@ -4,6 +4,13 @@ import matplotlib.pyplot as plt
 import util
 import torch.nn.functional as F
 import torch.tensor
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import datasets, transforms
+
+
+# Writer will output to ./runs/ directory by default
+writer = SummaryWriter()
+
 
 batch_size_test = 1000
 
@@ -71,6 +78,48 @@ def printnorm(self, input, output):
     print('output size:', output.data.size())
     print('output norm:', output.data.norm())
 
+def printnorm_fc2_prune(self, input, output):
+    # input is a tuple of packed inputs
+    # output is a Tensor. output.data is the Tensor we are interested
+    print('Inside ' + self.__class__.__name__ + ' forward')
+    print('')
+    print('input: ', type(input))
+    print('input[0]: ', type(input[0]))
+    print(input)
+    print('output: ', type(output))
+    print('output[0]: ', type(output[0]))
+    print('output.data:', output.data)
+#    print('output[0].data:', output[0].data)
+    print('')
+    print('input size:', input[0].size())
+    print('output size:', output.data.size())
+    print('output norm:', output.data.norm())
+    print('!!!! copy tensor')
+    global fc2_prune_input
+    fc2_prune_input = input[0].clone().detach()
+
+def printnorm_fc2_orig(self, input, output):
+    # input is a tuple of packed inputs
+    # output is a Tensor. output.data is the Tensor we are interested
+    print('Inside ' + self.__class__.__name__ + ' forward')
+    print('')
+    print('input: ', type(input))
+    print('input[0]: ', type(input[0]))
+    print(input)
+    print('output: ', type(output))
+    print('output[0]: ', type(output[0]))
+    print('output.data:', output.data)
+#    print('output[0].data:', output[0].data)
+    print('')
+    print('input size:', input[0].size())
+    print('output size:', output.data.size())
+    print('output norm:', output.data.norm())
+    print('!!!! copy tensor')
+    global fc2_orig_input
+    fc2_orig_input = input[0].clone().detach()
+
+
+
 def printnorm_fc3_prune(self, input, output):
     # input is a tuple of packed inputs
     # output is a Tensor. output.data is the Tensor we are interested
@@ -113,7 +162,7 @@ def printnorm_fc3_orig(self, input, output):
     
 
 model.fc1.register_forward_hook(printnorm)
-model.fc2.register_forward_hook(printnorm)
+model.fc2.register_forward_hook(printnorm_fc2_prune)
 model.fc3.register_forward_hook(printnorm_fc3_prune)
 #
 
@@ -142,7 +191,7 @@ print(model_orig)
 util.print_model_parameters(model_orig)
 
 model_orig.fc1.register_forward_hook(printnorm)
-model_orig.fc2.register_forward_hook(printnorm)
+model_orig.fc2.register_forward_hook(printnorm_fc2_orig)
 model_orig.fc3.register_forward_hook(printnorm_fc3_orig)
 
 result_orig = model_orig(example_data[0])
@@ -156,6 +205,22 @@ print("\n")
 
 
 #print('fc3input type:', type(fc3input))
+print('fc2_prune_input :', fc2_prune_input)
+print('fc2_orig_input :', fc2_orig_input)
+
+fc2_input_diffabs = torch.abs(fc2_orig_input - fc2_prune_input);
+
+print('fc2_input_diffabs :', fc2_input_diffabs)
+
+
+#fc3_input_diffabs_histc = torch.histc(fc3_input_diffabs, 100, 0, 100)
+#print('fc3_input_diffabs_histc :', fc3_input_diffabs_histc)
+
+writer.add_histogram('fc2/prune_input', fc2_prune_input, bins='tensorflow')
+writer.add_histogram('fc2/orig_input', fc2_orig_input, bins='tensorflow')
+writer.add_histogram('fc2/input_diffabs', fc2_input_diffabs, bins='tensorflow')
+
+
 print('fc3_prune_input :', fc3_prune_input)
 print('fc3_orig_input :', fc3_orig_input)
 
@@ -164,8 +229,41 @@ fc3_input_diffabs = torch.abs(fc3_orig_input - fc3_prune_input);
 print('fc3_input_diffabs :', fc3_input_diffabs)
 
 
+#fc3_input_diffabs_histc = torch.histc(fc3_input_diffabs, 100, 0, 100)
+#print('fc3_input_diffabs_histc :', fc3_input_diffabs_histc)
+
+writer.add_histogram('fc3/prune_input', fc3_prune_input, bins='tensorflow')
+writer.add_histogram('fc3/orig_input', fc3_orig_input, bins='tensorflow')
+writer.add_histogram('fc3/input_diffabs', fc3_input_diffabs, bins='tensorflow')
+writer.close()
 
 
+
+#def count_elements(seq) -> dict:
+#    """Tally elements from `seq`."""
+#    hist = {}
+#    for i in seq:
+#        hist[i] = hist.get(i, 0) + 1
+#    return hist
+#
+#
+#def ascii_histogram(seq) -> None:
+#    """A horizontal frequency-table/histogram plot."""
+#    counted = count_elements(seq)
+#    for k in sorted(counted):
+#        print('{0:5d} {1}'.format(k, '+' * counted[k]))
+#        
+#
+#ascii_histogram(fc3_input_diffabs_histc.data)
+
+
+
+def ascii_histogram(seq) -> None:
+    """A horizontal frequency-table/histogram plot."""
+    for k in seq:
+        print('{0:5d} {1}'.format(k, '+' * k))
+
+#ascii_histogram(fc3_input_diffabs_histc.long())
 
 
 
